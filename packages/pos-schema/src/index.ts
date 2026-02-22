6f7f77ff4a266ef7b0e64008eab0b05fe049e454
SithumManusha <appuhamysithum@gmail.com>
2026-02-22 12:22:39 +0545

    Revert "feat: complete offline sync module"

--- packages/pos-schema/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { PushSchema, RetrySchema, SettingSchema } from './sync.schema.js';
-export { PushSchema, RetrySchema, SettingSchema };
-export type { PushDto, RetryDto, SettingDto } from './sync.schema.js';
