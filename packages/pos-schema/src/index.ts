03bf5874d4e52117f3c19fa2909bd9501ffaecd0
Senaya Amanethmi Mendis <senayamendis2@gmail.com>
2026-04-20 09:51:31 +0530

    Restore missing files before rebase

--- packages/pos-schema/src/index.ts
+++ packages/pos-schema/src/index.ts
@@ -4,5 +4,6 @@ export * from './product.schema';
 export * from './supplier.schema';
 export * from './inventory.schema';
 export * from './purchase-order.schema';
-export * from './transfer.schema';
 export * from './batch.schema';
+export * from './auth.schema';
+export * from './transfer.schema';
