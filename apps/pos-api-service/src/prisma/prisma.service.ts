e3b0833f025492b3c2721a585a9d5991606425a2
Hansani17 <ishankapallewela071@gmail.com>
2026-04-22 05:56:58 +0530

    Restore lost files from commit 3ef8617

--- apps/pos-api-service/src/prisma/prisma.service.ts
+++ apps/pos-api-service/src/prisma/prisma.service.ts
@@ -1,25 +1,29 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@ryzera/pos-database';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
+import pg from 'pg';
+import 'dotenv/config';
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+const adapter = new PrismaPg(pool);
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    const connectionString = process.env.DATABASE_URL;
-    if (!connectionString) throw new Error('DATABASE_URL missing');
-    
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    
-    super({ adapter });
-  }
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy
+{
+    constructor() {
+        super({ adapter } as any);
+    }
 
-  async onModuleInit() {
-    await this.$connect();
-  }
+    async onModuleInit() {
+        await this.$connect();
+    }
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+    async onModuleDestroy() {
+        await this.$disconnect();
+        await pool.end();
+    }
 }
